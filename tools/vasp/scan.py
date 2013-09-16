b5e8c02cf3c81a0ccc823e60f20d62b5576cd8fa
Tao Cheng <chengtaoliterature@gmail.com>
2013-09-15 23:17:21 -0700

    bug fix fun2008; add vasp tools

---
+++ tools/vasp/scan.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+
+lines = []
+f = open("POSCAR", "r")
+for i in f:
+    lines.append(i) 
+f.close()
+
+start = 0.80
+start = np.power(start, 1/3.0)
+end = 1.20
+end = np.power(end, 1/3.0)
+n = 11
+
+x = np.linspace(start, end, n)
+
+for i in range(n):
+    folder = "scan_%02d"%i
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    os.chdir(folder)
+    o = open("POSCAR", "w")
+    lines[1] = "    %.4f\n"%x[i]
+    for j in lines:
+        o.write(j)
+    o.close()
+    os.chdir("..")
