56b3fca0667e0a1fdac91e050b0b4eb8b32e0a28
Tao Cheng <chengtaoliterature@gmail.com>
2014-04-18 13:27:27 -0700

    scan.py for gaussian with angle scan

--- tools/g03/scan.py
+++ tools/g03/scan.py
@@ -5,16 +5,47 @@ import os
 import shutil
 import sys
 import numpy as np
+import argparse
 
-def usage():
-    print """scan.py inputfile min max n
-    inputfile: model file
-    min: the min value
-    max: the max value
-    n: number of data points
-    """
+def angle_scan(fname, opt, min, max, n):
+    lines = []
+    f = open(fname, "r")
+    for i in f:
+        if i.startswith("A"):
+            tokens = i.strip().split()
+            tokens[4] = "%angle%"
+            line = " ".join(tokens)
+        else:
+            line = i.strip() + '\n'
+        lines.append(line)
+    f.close()
 
-def main(fname, opt, min, max, n):
+    angles = []
+    flag = 0
+    for i in np.linspace(min, max, n):
+        if opt < i and flag == 0:
+            angles.append(opt)
+            flag = 1
+        angles.append(i)
+    print angles
+    
+    oangle = open("angles", "w")
+    for i in range(len(angles)):
+        o = open("scan_%02d.gjf"%i, "w")
+        angle = angles[i]
+        for j in lines:
+            j = j.replace("%angle%", "%.6f"%angle)
+            o.write(j)
+        o.write("\n")
+        o.write("\n")
+        o.close()
+        oangle.write("%12.6f\n"%angle)
+        if not os.path.exists("scan_%02d"%i):
+            os.mkdir("scan_%02d"%i)
+        shutil.copy("scan_%02d.gjf"%i, "scan_%02d"%i)
+    oangle.close()
+
+def bond_scan(fname, opt, min, max, n):
     lines = []
     f = open(fname, "r")
     for i in f:
@@ -55,16 +86,39 @@ def main(fname, opt, min, max, n):
         shutil.copy("scan_%02d.gjf"%i, "scan_%02d"%i)
     obond.close()
 
-if __name__ == "__main__":
-    if len(sys.argv) < 5:
-        usage()
-    else:
-        fname = sys.argv[1]
-        opt = float(sys.argv[2])
-        min = float(sys.argv[3])
-        max = float(sys.argv[4])
-        if len(sys.argv) == 6:
-            n = int(sys.argv[5])
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fname", nargs=1, help="g03 file")
+    parser.add_argument("-params", nargs="+", type=float, help="Parameters for scan: opt, min, max, n")
+    parser.add_argument("-type", default="bond", nargs=1, help="Scan type: bond or angle")
+    args = parser.parse_args()
+    
+    fname = args.fname[0]
+    print "Processing %s"%fname
+    if args.params:
+        tokens = args.params
+        if len(tokens) < 3:
+            print "Need at least 3 paramester. Only %d input."%len(tokens)
+            exit()
         else:
-            n = 3
-        main(fname,opt, min, max, n)
+            opt = tokens[0]
+            min = tokens[1]
+            max = tokens[2]
+            print "opt = %.2f, min = %.2f, max = %.2f"%(opt, min, max),
+            if len(tokens) > 3:
+                n = tokens[3]
+                print "n = %d"%n
+            else:
+                print ""
+
+    if args.type:
+        type = args.type[0]
+    print "%s scan"%type
+    print n
+    if type == "bond": 
+        bond_scan(fname,opt, min, max, n)
+    elif type == "angle":
+        angle_scan(fname, opt, min, max, n)
+
+if __name__ == "__main__":
+    main()
