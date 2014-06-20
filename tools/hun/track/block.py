29aaff691cc2aac84881d6ff90161d55818f5149
Tao Cheng <chengtaoliterature@gmail.com>
2014-06-19 20:47:22 -0700

    track the species

---
+++ tools/hun/track/block.py
@@ -0,0 +1,140 @@
+"""parse the integrated file into seperated single files
+@bug : the first geo missing BIOGRF header
+"""
+
+import argparse
+
+def dumpBlock(dumpfile, outfile="dump.sep", dt = 1):
+    """parse the dump file into blocks
+    @return: the number of frames in dump file.
+    """
+    counter = 0
+    tag = 0
+    f = open(dumpfile, 'r')
+    lines = ''
+    block = []
+    for i in f:
+        if counter > 0 and "TIMESTEP" in i:
+            if tag % dt == 0:
+                o = open(outfile+"%05d"%tag+".dump", 'w')
+                for j in block:
+                    o.write(j)
+                o.close()
+            block = []
+            tag += 1
+        block.append(i)
+        counter += 1
+    o = open(outfile+"%05d"%tag+".dump", 'w')
+    for j in block:
+        o.write(j)
+    o.close()
+    f.close()
+    return tag
+
+def geoBlock(geofile, ext="geo"):
+    """parse the geo file into blocks
+    """
+    f = open(geofile, 'r')
+    lines = ''
+    block = []
+    out = ''
+    counter = 0
+    for i in f:
+        if counter > 0:
+            if "BIOGRF" in i:
+                o = open("%s"%out + ".%s"%ext, 'w')
+                for j in block:
+                    o.write(j)
+                o.close()
+                block = []
+            elif "XTLGRF" in i:
+                o = open("%s"%out + ".%s"%ext, 'w')
+                for j in block:
+                    o.write(j)
+                o.close()
+                block = []
+            elif "DESCRP" in i:
+                out = i.strip().split()[-1]
+            block.append(i)
+        if counter == 0:
+            block.append(i)
+        counter += 1
+    o = open("%s"%out + ".%s"%ext, 'w')
+    for j in block:
+        o.write(j)
+    o.close()
+
+def xyzBlock(xyzfile, n, outfile="output", dt=1):
+    """parse the xyz file into blocks
+    @return: the number of frames in dump file.
+    """
+    counter = 0
+    tag = 0
+    f = open(xyzfile, 'r')
+    lines = ''
+    block = []
+    for i in f:
+        if counter > 0 and counter%n == 0: 
+            if tag % dt == 0:
+                o = open(outfile+"%05d"%tag+".xyz", 'w')
+                for j in block:
+                    o.write(j)
+                o.close()
+            block = []
+            tag += 1
+        block.append(i)
+        counter += 1
+    o = open(outfile+"%05d"%tag+".xyz", 'w')
+    for j in block:
+        o.write(j)
+    o.close()
+    f.close()
+    return tag
+
+def g03Block(g03file, ext="log"):
+    """parse the g03 log file into blocks
+    """
+    head = []
+    block_prev = []
+    block_now = []
+    f = open(g03file, 'r')
+
+    for i in f:
+        if i.strip().startswith("Berny optimization"):
+            break
+        head.append(i)
+
+    block_now.append(i)
+
+    counter = 0
+    flag = 1
+    while flag:
+        flag = 0
+        for i in f:
+            flag = 1
+            if i.strip().startswith("Berny optimization"):
+                block_prev = block_now
+                block_now = []
+                break
+            if i.strip().startswith("Step number   1"):
+                ofile = "%s_%02d.%s"%(g03file.split(".")[0], counter, ext)
+                o = open(ofile, "w")
+                for j in block_prev:
+                    o.write(j)
+                o.close()
+                counter += 1
+            block_now.append(i)
+
+    ofile = "%s_%02d.%s"%(g03file.split(".")[0], counter, ext)
+    o = open(ofile, "w")
+    for j in block_prev:
+        o.write(j)
+    o.close()
+
+    print counter
+
+
+if __name__ == "__main__":
+
+    n = 210
+    xyzBlock("movie.xyz", n)
