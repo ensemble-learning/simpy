59d1e51f828d19c05e87f945936fdfc94beb8e25
Tao Cheng <chengtaoliterature@gmail.com>
2014-11-20 15:00:49 -0800

    debug msd.py

--- tools/vasp/gen.sh
+++ tools/vasp/gen.sh
@@ -1,4 +1,9 @@
 python ~/soft/simpy/tools/vasp/get_potcar.py
 python ~/soft/simpy/tools/vasp/get_kpoint.py
 python ~/soft/simpy/tools/vasp/incar.py
-genpbs_vasp
+
+# for vasp 5.2
+#genpbs_vasp
+
+# for vasp 5.3
+python /project/source/VASP/vasp.5.3.5/bin/genVASPpbs.py 1 4 24:00:00 vasp
