c25c56b00927253c810cd512988b2b46cd4b93a2
Tao Cheng <chengtaoliterature@gmail.com>
2016-10-15 03:50:03 -0700

    update

--- lib/e_2_dump.py
+++ lib/e_2_dump.py
@@ -1,6 +1,6 @@
 from block import dumpBlock
 from dump import Dump
-from output_conf import toXyz, toPdb
+from output_conf import toXyz, toPdb, toPOSCAR
 
 lmpfile = "dump.lammpstrj"
 sepfile = "dump.sep"
@@ -15,9 +15,14 @@ for i in range(0, nframe, dt):
 #for i in range(10):
     a = Dump("%s%05d.dump"%(sepfile,i))
     b = a.parser()
+    b.assignEleTypes()
+    b.toFrac()
+
     toXyz(b, "xyz%05d.xyz"%i)
     #b.sortXYZ("z")
     toPdb(b, "pdb%05d.pdb"%i)
+    toReaxLammps(b, "lammps.data")
+    toPoscar(b,)
 
 
 
