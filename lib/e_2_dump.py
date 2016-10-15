9cd863682a0fb9d46483c69bf2dbdc1b2fdf59f6
Tao Cheng <chengtaoliterature@gmail.com>
2016-10-15 03:52:39 -0700

    update

--- lib/e_2_dump.py
+++ lib/e_2_dump.py
@@ -1,6 +1,6 @@
 from block import dumpBlock
 from dump import Dump
-from output_conf import toXyz, toPdb, toPoscar
+from output_conf import toXyz, toPdb, toPoscar, toReaxLammps
 
 lmpfile = "dump.lammpstrj"
 sepfile = "dump.sep"
