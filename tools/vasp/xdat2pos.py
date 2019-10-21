1c4a76da2668575ed282ab6666f6baeac5d0f235
tcheng <tcheng@suda.edu.cn>
2019-10-21 10:19:23 +0800

    update

--- tools/vasp/xdat2pos.py
+++ tools/vasp/xdat2pos.py
@@ -33,6 +33,8 @@ elif "armstrong" in socket.gethostname():
     LIB = "/p/home/taocheng/src/simpy/lib"
 elif "stampede2" in socket.gethostname():
     LIB = "/home1/04076/tg833760/soft/simpy/lib"
+elif socket.gethostname() == "mu-01":
+    LIB = "/opt/sourcecoude/simpy/lib"
 elif "tao-Precision-Tower-3420-ubuntu" in socket.gethostname():
     LIB = "/home/tao/soft/simpy/lib"
 
